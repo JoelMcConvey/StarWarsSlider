a187a707bbd127ee1bcc7f34a4ca6bb0c78be748
JoelMcConvey <83514500+JoelMcConvey@users.noreply.github.com>
2023-05-09 18:13:02 +0100

    Add files via upload

---
+++ src/images.js
@@ -0,0 +1,12 @@
+import image1 from './assets/luke-skywalker.jpeg'
+import image2 from './assets/princess-leia.webp'
+import image3 from './assets/darth-vader.jpeg'
+import image4 from './assets/han-solo.jpeg'
+import image5 from './assets/chewbacca.jpg'
+import image6 from './assets/yoda.webp'
+import image7 from './assets/obi-wan.jpeg'
+import image8 from './assets/r2d2-c3po.webp'
+import image9 from './assets/tarkin.webp'
+import image10 from './assets/emperor.jpg'
+
+export default [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10];
